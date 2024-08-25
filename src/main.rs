cbb7d6669be334a6ca40a4c6d87b5a7c577b8cef
Syed Ahkam <smahkam57@gmail.com>
2024-08-25 23:43:42 +0530

    feat: bevy flycam

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use bevy::{
-    prelude::*,
-    window::PresentMode,
-};
+use bevy::{app::Plugins, prelude::*, window::PresentMode};
+use bevy_flycam::prelude::*;
 
 const APP_NAME: &str = env!("CARGO_PKG_NAME");
 const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -14,45 +12,46 @@ fn main() {
                 resolution: (700., 500.).into(),
                 present_mode: PresentMode::AutoVsync,
                 // Tells wasm to resize the window according to the available canvas
-                fit_canvas_to_parent: true,
+                // fit_canvas_to_parent: true,
                 // Tells wasm not to override default event handling, like F5, Ctrl+R etc.
-                prevent_default_event_handling: false,
+                // prevent_default_event_handling: false,
                 ..default()
             }),
             ..default()
         }))
+        .add_plugins(PlayerPlugin)
         .add_systems(Startup, setup)
         .run();
 }
 
-fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<StandardMaterial>>) {
-    println!("Starting {} {}", APP_NAME, APP_VERSION);
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn((PbrBundle {
+        mesh: meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(20.5))),
+        material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
+        ..Default::default()
+    },));
 
+    // cube
     commands.spawn(PbrBundle {
-        mesh: meshes.add(shape::Plane::from_size(5.0).into()),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+        material: materials.add(Color::srgb(0.8, 0.7, 0.6)),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
+        ..Default::default()
     });
 
+    // light
     commands.spawn(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
+        ..Default::default()
     });
 
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    info!("Move camera around by using WASD for lateral movement");
+    info!("Use Left Shift and Spacebar for vertical movement");
+    info!("Use the mouse to look around");
+    info!("Press Esc to hide or show the mouse cursor");
 }
